4d823a06fe192258c633230faae361e4cb68e423
roesnera <adam.roesner@generation.org>
2023-05-01 13:35:32 -0400

    initial components created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from './components/Home.jsx';
+import About from './components/About.jsx';
+import ArticlePage from './components/ArticlePage.jsx';
+
+export default function App() {
+
+    return <div>
+        <Home />
+        <About />
+        <ArticlePage />
+    </div>
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+const el = document.getElementById('root');
+
+const root = ReactDOM.createRoot(el);
+
+root.render(
+    <App />
+);
