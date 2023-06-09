ec2bd0bb20a03ccf4393fc96efd757a2d7be048d
roesnera <adam.roesner@generation.org>
2023-06-09 13:23:19 -0400

    updates tests to leverage closures and reduce async function calls

--- test/supabase.test.js
+++ test/supabase.test.js
@@ -3,25 +3,30 @@ import { snagPosts, fetchMainArt, formatDate, getArticlesByAuthor } from "../uti
 
 describe("Supabase Client", function () {
     describe("snagPosts", function () {
+        let articles;
         it("Should return a non-empty list of articles", async function () {
-            const articles = await snagPosts();
+            articles = articles?? await snagPosts();
             assert.isTrue(articles.length>0);
         });
         it("Articles should have all and only title, author, text, created_at, post_id, and creator_account fields", async function (){
-            const articles = await snagPosts();
+            articles = articles?? await snagPosts();
             const articleProps = Object.keys(articles[0]);
             assert.hasAllKeys(articles[0], ["title", "author", "text", "created_at", "post_id", "creator_account"]);
         });
     });
     describe("fetchMainArt", function () {
+        let article;
         it("should fetch only one article", async function() {
-            const article = await fetchMainArt();
+            article = article?? await fetchMainArt();
             assert.isNotArray(article);
             assert.isObject(article);
         });
         it("Article should have all and only title, author, text, created_at, post_id, and creator_account fields", async function (){
-            const article = await fetchMainArt();
+            article = article?? await fetchMainArt();
             assert.hasAllKeys(article, ["title", "author", "text", "created_at", "post_id", "creator_account"]);
         });
+    });
+    describe("formatDate", function () {
+        
     })
 })
